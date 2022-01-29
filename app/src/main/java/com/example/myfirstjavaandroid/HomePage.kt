f054bfcb2250ae8a11569b379a84aa12d2df6592
Pertisth Singh <pertisthsingh@gmail.com>
2022-01-29 09:53:47 +0530

    Used Shared Preferences

--- app/src/main/java/com/example/myfirstjavaandroid/HomePage.kt
+++ app/src/main/java/com/example/myfirstjavaandroid/HomePage.kt
@@ -5,31 +5,60 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.provider.AlarmClock
 import android.view.View
+import android.widget.EditText
 import android.widget.TextView
 import android.widget.Toast
 import com.example.myfirstjavaandroid.data.Datasource
 
+
+
 class HomePage : AppCompatActivity() {
+
+    lateinit var etMail :EditText;
+    lateinit var etPhone : EditText;
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home_page)
 
-        val textView: TextView = findViewById(R.id.textView)
-        textView.text = Datasource().loadAffirmations().size.toString()
+        etMail = findViewById(R.id.emailAddress);
+        etPhone = findViewById(R.id.phoneNumber);
+
     }
 
-    fun onClickHandler(view: View) {
-        createAlarm("Wake Up",14,48);
+    override fun onPause() {
+        super.onPause()
+        storeState();
     }
-    fun createAlarm(message: String, hour: Int, minutes: Int) {
-        val intent = Intent(AlarmClock.ACTION_SET_ALARM).apply {
-            putExtra(AlarmClock.EXTRA_MESSAGE, message)
-            putExtra(AlarmClock.EXTRA_HOUR, hour)
-            putExtra(AlarmClock.EXTRA_MINUTES, minutes)
-        }
-        if (intent.resolveActivity(packageManager) != null) {
-            startActivity(intent)
-        }
+
+    private fun storeState() {
+        var email: String = etMail.text.toString();
+        var phone : String = etPhone.text.toString();
+
+        var sharePreferences = getSharedPreferences("home_state_prefs", MODE_PRIVATE);
+
+        var editor = sharePreferences.edit();
+
+        editor.putString("email",email);
+        editor.putString("phone",phone);
+
+        editor.apply();
     }
+
+    override fun onResume() {
+        super.onResume()
+        restoreState();
+    }
+
+    private fun restoreState() {
+        var sharedPreferences = getSharedPreferences("home_state_prefs", MODE_PRIVATE);
+
+        var email = sharedPreferences.getString("email","");
+        var phone = sharedPreferences.getString("phone","");
+
+        etMail.setText(email);
+        etPhone.setText(phone);
+    }
+
 
 }
